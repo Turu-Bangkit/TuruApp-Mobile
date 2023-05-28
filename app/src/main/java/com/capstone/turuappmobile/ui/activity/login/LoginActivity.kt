bc222c0d30b8779b5c0d3996c169b277dbb43905
Dandy-Candra <106506074+Dandy-Candra@users.noreply.github.com>
2023-05-28 20:27:41 +0700

    Merging

--- app/src/main/java/com/capstone/turuappmobile/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/turuappmobile/ui/activity/login/LoginActivity.kt
@@ -81,6 +81,7 @@ class LoginActivity : AppCompatActivity() {
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
+
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     updateUI(null)
                 }
